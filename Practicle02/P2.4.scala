78e4a90e0a7550d1bb79034929967f709d03cd73
ndhs2023uoc <139251942+ndhs2023uoc@users.noreply.github.com>
2024-06-23 23:41:43 +0530

    Delete Practicle02 directory because commits are commented

--- Practicle02/P2.4.scala
+++
@@ -1,5 +0,0 @@
-def attendance(price: Int): Double = return 120 + ((15 - price) / 5) * 20.0
-
-def totalCost(price: Int): Double = return attendance(price)*3 +500
-
-def profit(price: Int): Double = return attendance(price)*price - totalCost(price)
